9f301c778de56e6df1da9399f1170d6e0ada8416
Joan Sebastian Tunubala Sanchez <jstunubala@gane.com.co>
2026-06-27 11:33:48 -0500

    Nuevas cosas

--- app/service/service.py
+++ app/service/service.py
@@ -5,8 +5,17 @@ class Pdfservice:
 
     def __init__(self, file) -> None:
         self.pdf = PdfReader(file)
+        self.new = PdfWriter()
 
-    @classmethod
     def get_title(self):
-        return self.pdf.metadata.title
+        return self.pdf.metadata.title if self.pdf.metadata else None
+    
+    def get_pages(self):
+        return len(self.pdf.pages) 
+
+    def estract_page_pdf(self, page):
+        input_page = self.pdf.pages[page]
+        return self.new.add_page(input_page)
+
+    
 
