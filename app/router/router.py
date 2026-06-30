f279d96635ceba1bff5872e44c6a0aa7322945b8
SebasTS15 <sebastiantunubala@gmail.com>
2026-06-29 20:07:33 -0500

    Creación de servicos y primera ruta

--- app/router/router.py
+++ app/router/router.py
@@ -1,3 +1,41 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile, HTTPException, Form
+from fastapi.responses import StreamingResponse
+from app.service.service import Pdfservice
 
 router = APIRouter()
+
+
+@router.post('/splitter_pdf')
+def splitter_pdf(
+    name_pdf: str = Form(),
+    pages_splitter: int = Form(), 
+    file: UploadFile = File()
+    ):
+
+    if file.content_type != 'application/pdf':
+        raise HTTPException(status_code=400, detail = 'El archivo debe ser un PDF')
+    
+    pdf_service = Pdfservice(file)
+    
+    try:
+        new_pdfs  =  pdf_service.splitter_pdf(pages_splitter)
+        zip_file = pdf_service.create_zip_pdf(new_pdfs,name_pdf)
+        return StreamingResponse(
+            zip_file,
+            media_type='application/zip',
+            headers={
+                "Content-Disposition": f'attachment; filename="{name_pdf}.zip"'
+                }
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+
+
+@goruter.get('/estrac_pdf')
+def estract_pd():
+    pass
+
+
--- app/service/service.py
+++ app/service/service.py
@@ -1,21 +1,104 @@
+from io import BytesIO
+import zipfile 
 from pypdf import PdfReader, PdfWriter
+import os
 
 
 class Pdfservice:
 
     def __init__(self, file) -> None:
         self.pdf = PdfReader(file)
-        self.new = PdfWriter()
 
     def get_title(self):
         return self.pdf.metadata.title if self.pdf.metadata else None
     
     def get_pages(self):
         return len(self.pdf.pages) 
+    
+    def validate_pages_per_pdf(self, pages_per_pdf: int):
+        total_pages = self.get_pages()
 
-    def estract_page_pdf(self, page):
-        input_page = self.pdf.pages[page]
-        return self.new.add_page(input_page)
+        if not isinstance(pages_per_pdf, int):
+            raise TypeError("El número de páginas por PDF debe ser un entero.")
+
+        if pages_per_pdf <= 0:
+            raise ValueError("El número de páginas por PDF debe ser mayor que cero.")
+
+        if pages_per_pdf > total_pages:
+            raise ValueError(
+                f"El PDF solo tiene {total_pages} páginas y no puede dividirse en bloques de {pages_per_pdf}."
+            )
+
+
+    
+    def creat_new_pdf(self,pages:list[int]):
+
+        for i in range(len(pages)):
+    
+            self.validate_pages_per_pdf(pages[i])
+
+        new_pdf = PdfWriter()
+
+        total_pages = len(self.pdf.pages)
+
+        pages = list(dict.fromkeys(pages))
+
+        for page in pages:
+            if not 1 <= page <= total_pages:
+                raise  ValueError(f'La pagina {page} no existe')
+            
+            new_pdf.add_page(self.pdf.pages[page - 1])
+
+        output = BytesIO()
+        new_pdf.write(output)
+        output.seek(0)
+
+        return output
+    
+    def splitter_pdf(self, pages_per_pdf: int):
+
+        self.validate_pages_per_pdf(pages_per_pdf)
+
+        total_pages = self.get_pages()
+
+        new_pdfs = []
+
+        for start_block in range(1, total_pages + 1, pages_per_pdf ):
+
+            new_pdf = PdfWriter()
+
+            end_block = start_block + pages_per_pdf - 1
+            
+            if end_block > total_pages:
+                end_block = total_pages
+                
+            for page_current in range(start_block, end_block + 1):
+
+                new_pdf.add_page(self.pdf.pages[page_current -1])
+
+            output = BytesIO()
+            new_pdf.write(output)
+            output.seek(0)
+            new_pdfs.append(output)
+        
+        return new_pdfs
+
+    def create_zip_pdf(new_pdfs: list[BytesIO], name_zip: str):
+
+        memoria_zip = BytesIO()
+
+        with zipfile.ZipFile(memoria_zip,'w',zipfile.ZIP_DEFLATED) as zip_f:
+            
+            for indice, archivo in enumerate(new_pdfs):
+
+                zip_f.writestr(f'{name_zip}_{indice+1}.pdf',archivo.getvalue())
+
+        memoria_zip.seek(0)
+        
+        return memoria_zip
+
+
+
 
     
 
